8fb86d2b0988e80c1c77f4f3cd52c4bf9a00c70a
Juan Ignacio Falibene <juanignaciofalibene@gmail.com>
2024-04-28 00:10:21 +0200

    options component intrgated

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,50 @@
+import React, { useState } from "react";
 import Stars from "./Stars";
+import Options from "./Options";
 
 function App() {
   const defaultRating = localStorage.getItem("starRating");
+
+  const [iconSize, setIconSize] = useState(50);
+  const [icon, setIcon] = useState("★");
+  const [color, setColor] = useState("#f6b73c");
+  const [count, setCount] = useState(5);
+
+  const handleIconSizeChange = (event) => {
+    setIconSize(parseInt(event.target.value));
+  };
+
+  const handleIconChange = (event) => {
+    setIcon(event.target.value);
+  };
+
+  const handleColorChange = (event) => {
+    setColor(event.target.value);
+  };
+
+  const handleCountChange = (event) => {
+    setCount(parseInt(event.target.value));
+  };
+
   return (
     <div>
-      <Stars iconSize={40} icon='☆' color='red' defaultRating={defaultRating} />
+      <Options
+        iconSize={iconSize}
+        icon={icon}
+        color={color}
+        count={count}
+        onIconSizeChange={handleIconSizeChange}
+        onIconChange={handleIconChange}
+        onColorChange={handleColorChange}
+        onCountChange={handleCountChange}
+      />
+      <Stars
+        iconSize={iconSize}
+        icon={icon}
+        color={color}
+        count={count}
+        defaultRating={defaultRating}
+      />
     </div>
   );
 }
